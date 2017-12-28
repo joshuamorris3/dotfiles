3a1c26345730cc503e3eb1d0c9455c2320b98ba6
Joshua Morris <josh@masteryconnect.com>
2017-12-28 21:31:38 +0000

    Fixed issues

--- bash/plugins/z.plugin.bash
+++
@@ -1,7 +0,0 @@
-# z
-if [ -f $DOTFILES/bash/bin/z/z.sh ]; then
-  . $DOTFILES/bash/bin/z/z.sh
-
-  echo $PROMPT_COMMAND | grep -q "z --add"
-  [ $? -gt 0 ] && PROMPT_COMMAND='z --add "$(pwd -P)";'"$PROMPT_COMMAND"
-fi
